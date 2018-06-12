0315f4ea6546844451cad9445fa178df6e74ee6f
freezy1 <mail@felixfritschi>
2018-06-12 11:15:06 +0200

    TransactionFeedbackActivity created

---
+++ app/src/main/java/com/dhbw/magicmoney/TransactionFeedbackActivity.java
@@ -0,0 +1,13 @@
+package com.dhbw.magicmoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_feedback);
+    }
+}
