4588e5cfa8c58b3626728726ea0b26a4c32d6fd9
freezy1 <mail@felixfritschi>
2018-06-12 15:49:40 +0200

    worked on TagReceivedActivity

--- app/src/main/java/com/dhbw/magicmoney/TagReceivedActivity.java
+++ app/src/main/java/com/dhbw/magicmoney/TagReceivedActivity.java
@@ -1,13 +1,129 @@
 package com.dhbw.magicmoney;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Build;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class TagReceivedActivity extends AppCompatActivity {
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class TagReceivedActivity extends AppCompatActivity implements NfcAdapter.OnNdefPushCompleteCallback, NfcAdapter.CreateNdefMessageCallback {
+
+    private ArrayList<String> dataToSendArray = new ArrayList<>();
+    private ArrayList<String> dataReceivedArray = new ArrayList<>();
+
+    private NfcAdapter mNfcAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_received);
+
+        handleNfcIntent(getIntent());
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(mNfcAdapter != null) {
+            //Handle some NFC initialization here
+        }
+        else {
+            Toast.makeText(this, "NFC not available on this device",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+
+        if (dataToSendArray.size() == 0) {
+            return null;
+        }
+
+        NdefRecord[] recordsToAttach = createRecords();
+
+        return new NdefMessage(recordsToAttach);
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        Toast.makeText(this, "NFC signal sent!", Toast.LENGTH_SHORT);
+
+    }
+
+    public NdefRecord[] createRecords(){
+        NdefRecord[] records = new NdefRecord[dataToSendArray.size() + 1];
+
+        //To Create Messages Manually if API is less than
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            for (int i = 0; i < dataToSendArray.size(); i++){
+                byte[] payload = dataToSendArray.get(i).
+                        getBytes(Charset.forName("UTF-8"));
+                NdefRecord record = new NdefRecord(
+                        NdefRecord.TNF_WELL_KNOWN,      //Our 3-bit Type name format
+                        NdefRecord.RTD_TEXT,            //Description of our payload
+                        new byte[0],                    //The optional id for our Record
+                        payload);                       //Our payload for the Record
+
+                records[i] = record;
+            }
+        }
+        //API is high enough that we can use createMime, which is preferred.
+        else {
+            for (int i = 0; i < dataToSendArray.size(); i++){
+                byte[] payload = dataToSendArray.get(i).
+                        getBytes(Charset.forName("UTF-8"));
+
+                NdefRecord record = NdefRecord.createMime("text/plain",payload);
+                records[i] = record;
+            }
+        }
+        records[dataToSendArray.size()] =
+                NdefRecord.createApplicationRecord(getPackageName());
+        return records;
+    }
+
+    private void handleNfcIntent(Intent NfcIntent) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(NfcIntent.getAction())) {
+            Parcelable[] receivedArray =
+                    NfcIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            if(receivedArray != null) {
+                dataReceivedArray.clear();
+                NdefMessage receivedMessage = (NdefMessage) receivedArray[0];
+                NdefRecord[] attachedRecords = receivedMessage.getRecords();
+
+                for (NdefRecord record:attachedRecords) {
+                    String string = new String(record.getPayload());
+
+                    if (string.equals(getPackageName())) { continue; }
+                    dataReceivedArray.add(string);
+                }
+                Toast.makeText(this, "Received " + dataReceivedArray.size() +
+                        " Data parts", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this, "Received Blank Parcel", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handleNfcIntent(getIntent());
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        handleNfcIntent(intent);
     }
 }
