47da6334558a032afe7571032eed5e624e5d4419
Nicolai <nipes@hotmail.de>
2018-06-21 22:42:35 +0200

    Anpassung Registrierungsoberfläche + Farbanpassungen

--- app/src/main/java/com/dhbw/magicmoney/RegisterActivity.java
+++ app/src/main/java/com/dhbw/magicmoney/RegisterActivity.java
@@ -34,16 +34,9 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Registrieren");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         usernameView = (EditText) findViewById(R.id.register_username);
         nameView = (EditText) findViewById(R.id.register_name);
